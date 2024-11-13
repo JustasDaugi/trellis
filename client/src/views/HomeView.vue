8a0423ac1f424cbc903da266c313ac205c07a2d3
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-13 14:16:45 +0200

    fix: comment-out blogging platform paragraph and header

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -8,11 +8,11 @@ import { isLoggedIn } from '@/stores/user'
     <div v-if="!isLoggedIn" class="rounded-md bg-white px-6 py-8">
       <div class="items-center lg:flex">
         <div class="lg:w-1/2">
-          <h2 class="text-4xl font-bold text-gray-800 dark:text-gray-100">Open Blogging Platform</h2>
-          <p class="mt-4 text-gray-500 dark:text-gray-400 lg:max-w-md">
+          <!-- <h2 class="text-4xl font-bold text-gray-800 dark:text-gray-100">Open Blogging Platform</h2> -->
+          <!-- <p class="mt-4 text-gray-500 dark:text-gray-400 lg:max-w-md">
             Hi! Join the community and start sharing your thoughts with the world. Sign up now
             to become a member.
-          </p>
+          </p> -->
           <div class="mt-6 flex items-center gap-2">
             <FwbButton component="RouterLink" tag="router-link" href="/signup">Sign Up</FwbButton>
             <FwbButton component="RouterLink" tag="router-link" color="alternative" href="/login">
