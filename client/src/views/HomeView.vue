8aec5c628b6d9a0e5074a2ae84058ed4ab81678e
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-19 13:22:47 +0200

    chore: adjust positioning of login and signup buttons

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,35 +4,48 @@ import { isLoggedIn } from '@/stores/user'
 </script>
 
 <template>
-  <div class="dark:bg-gray-800">
+  <div class="flex min-h-screen items-center justify-center dark:bg-gray-800">
     <div v-if="!isLoggedIn" class="rounded-md bg-white px-6 py-8">
-      <div class="items-center lg:flex">
-        <div class="lg:w-1/2">
-          <!-- <h2 class="text-4xl font-bold text-gray-800 dark:text-gray-100">Open Blogging Platform</h2> -->
-          <!-- <p class="mt-4 text-gray-500 dark:text-gray-400 lg:max-w-md">
-            Hi! Join the community and start sharing your thoughts with the world. Sign up now
-            to become a member.
-          </p> -->
-          <div class="mt-6 flex items-center gap-2">
-            <FwbButton component="RouterLink" tag="router-link" href="/signup">Sign Up</FwbButton>
-            <FwbButton component="RouterLink" tag="router-link" color="alternative" href="/login">
-              Log In
-            </FwbButton>
-          </div>
+      <div class="flex flex-col items-center">
+        <p
+          class="mt-4 whitespace-nowrap text-center font-poppins font-bold italic text-gray-500 dark:text-gray-400"
+        >
+          Drive success with your team, anytime.
+        </p>
+
+        <div class="mb-40 mt-6 flex flex-col items-center gap-2">
+          <FwbButton
+            class="w-56 text-center"
+            component="RouterLink"
+            tag="router-link"
+            href="/signup"
+          >
+            Sign Up
+          </FwbButton>
+          <FwbButton
+            class="w-56 text-center"
+            component="RouterLink"
+            tag="router-link"
+            color="alternative"
+            href="/login"
+          >
+            Log In
+          </FwbButton>
         </div>
-        <div class="mt-8 lg:mt-0 lg:w-1/2">
-          <div class="flex items-center justify-center lg:justify-end">
-            <div class="max-w-lg">
-              <!-- <picture>
-                <source srcset="../assets/illustration.webp" type="image/webp" />
-                <img
-                  class="h-64 w-full rounded-md object-cover object-center"
-                  src="../assets/illustration.png"
-                  alt="Community illustration"
-                />
-              </picture> -->
-            </div>
-          </div>
+      </div>
+
+      <div class="mt-8 flex items-center justify-center">
+        <div class="max-w-lg">
+          <!--
+          <picture>
+            <source srcset="../assets/illustration.webp" type="image/webp" />
+            <img
+              class="h-64 w-full rounded-md object-cover object-center"
+              src="../assets/illustration.png"
+              alt="Community illustration"
+            />
+          </picture>
+          -->
         </div>
       </div>
     </div>
