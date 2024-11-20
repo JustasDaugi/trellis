308c458c9d9d95afbf19def8f5cbc962b31aaada
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-20 11:30:32 +0200

    feat: add templates route

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,13 +3,15 @@ import board from './board'
 import user from './user'
 import list from './list'
 import card from './card'
+import templates from './templates'
 
 
 export const appRouter = router({
   board,
   list,
   card,
-  user, 
+  user,
+  templates,
 })
 
 export type AppRouter = typeof appRouter
