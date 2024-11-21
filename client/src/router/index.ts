c2ad470f333cef7fd80946c8cb8b8dc09e04f51c
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-21 11:47:12 +0200

    fix: add middleware to router for redirecting

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,38 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import MainLayout from '@/layouts/MainLayout.vue'
-import { authenticate } from './guards'
+import { isLoggedIn } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/dashboard',
-      component: MainLayout,
-      beforeEnter: [authenticate],
+      name: 'MainView',
+      component: () => import('../views/MainView.vue'),
     },
     {
       path: '/login',
       name: 'Login',
-      component: () => import('@/views/LoginView.vue'),
+      component: () => import('../views/LoginView.vue'),
     },
     {
       path: '/signup',
       name: 'Signup',
-      component: () => import('@/views/SignupView.vue'),
+      component: () => import('../views/SignupView.vue'),
     },
     {
       path: '/',
-      component: MainLayout,
-      children: [
-        {
-          path: '',
-          name: 'Home',
-          component: HomeView,
-        },
-      ],
+      name: 'Home',
+      component: HomeView,
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (isLoggedIn.value) {
+    if (to.name === 'Login' || to.name === 'Signup' || to.name === 'Home') {
+      next({ name: 'MainView' })
+    } else {
+      next()
+    }
+  } else {
+    if (to.name === 'MainView' || to.path.startsWith('/dashboard')) {
+      next({ name: 'Home' })
+    } else {
+      next()
+    }
+  }
 })
 
 export default router
--- logging.md
+++
@@ -1,4 +0,0 @@
-What should be logged? 
-
-We recommend starting lean and clean - a few key logs before and after some "error-prone" operations, such as establishing a database connection, performing external requests, etc.
-
