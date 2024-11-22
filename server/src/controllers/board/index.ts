79e4ec260a5eba839e8c91611443e77e89d3ba73
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-22 11:18:06 +0200

    feat: add get file to router namespace

--- server/src/controllers/board/index.ts
+++ server/src/controllers/board/index.ts
@@ -1,12 +1,14 @@
 import { router } from '@server/trpc'
 import create from './create'
 import findAll from './findAll'
+import get from './get'
 import deleteById from './deleteById'
 import update from './update'
 
 export default router({
   create,
   findAll,
+  get,
   deleteById,
   update
 })
