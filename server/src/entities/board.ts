95c11639883c170fba1403146e5a95b16b5e3f50
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-22 11:19:50 +0200

    feat: remove unused properties, add selectedBackground property

--- server/src/entities/board.ts
+++ server/src/entities/board.ts
@@ -6,19 +6,13 @@ import { idSchema } from './shared'
 export const boardSchema = z.object({
   id: idSchema,
   title: z.string().min(1).max(500),
-  imageId: z.string().nullable(),
-  imageThumbUrl: z.string().nullable(),
-  imageFullUrl: z.string().nullable(),
-  imageUserName: z.string().nullable(),
-  imageLinkHtml: z.string().nullable(),
+  selectedBackground: z.string().nullable(),
   createdAt: z.date().default(() => new Date()),
   updatedAt: z.date().default(() => new Date()),
   userId: idSchema,
 })
 
-export const boardKeysAll = Object.keys(
-  boardSchema.shape
-) as (keyof Board)[]
+export const boardKeysAll = Object.keys(boardSchema.shape) as (keyof Board)[]
 
 export const boardKeysPublic = boardKeysAll
 
