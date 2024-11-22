8199cc3f8dc27f7daf39e391438aab1b472baf56
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-22 11:13:09 +0200

    chore: change column names from camel case to snake case

--- server/src/database/migrations/20241023T094842Z-createListTable.ts
+++ server/src/database/migrations/20241023T094842Z-createListTable.ts
@@ -8,28 +8,28 @@ export async function up(db: Kysely<any>) {
     )
     .addColumn('title', 'text', (c) => c.notNull())
     .addColumn('order', 'integer')
-    .addColumn('boardId', 'integer', (c) =>
+    .addColumn('board_id', 'integer', (c) =>
       c.references('board.id').onDelete('cascade').notNull()
     )
-    .addColumn('userId', 'integer', (c) =>
+    .addColumn('user_id', 'integer', (c) =>
       c.references('user.id').onDelete('set null').notNull()
     )
-    .addColumn('createdAt', 'timestamptz', (c) =>
+    .addColumn('created_at', 'timestamptz', (c) =>
       c.defaultTo(sql`CURRENT_TIMESTAMP`).notNull()
     )
-    .addColumn('updatedAt', 'timestamptz', (c) =>
+    .addColumn('updated_at', 'timestamptz', (c) =>
       c.defaultTo(sql`CURRENT_TIMESTAMP`).notNull()
     )
     .execute()
 
   await db.schema
-    .createIndex('idx_list_boardId')
+    .createIndex('idx_list_board_id')
     .on('list')
-    .column('boardId')
+    .column('board_id')
     .execute()
 }
 
 export async function down(db: Kysely<any>) {
-  await db.schema.dropIndex('idx_list_boardId').on('list').execute()
+  await db.schema.dropIndex('idx_list_board_id').on('list').execute()
   await db.schema.dropTable('list').execute()
 }
