f30f6cae49873b85979ce746c2f60339c5b2df60
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-22 20:46:02 +0200

    fix: change column name to selectedBackground

--- server/src/database/migrations/20241023T093911Z-createBoardTable.ts
+++ server/src/database/migrations/20241023T093911Z-createBoardTable.ts
@@ -7,7 +7,7 @@ export async function up(db: Kysely<any>) {
       c.primaryKey().generatedAlwaysAsIdentity()
     )
     .addColumn('title', 'text', (c) => c.notNull())
-    .addColumn('selected_background', 'text')
+    .addColumn('selectedBackground', 'text')
     .addColumn('created_at', 'timestamptz', (c) =>
       c.notNull().defaultTo(sql`now()`)
     )
