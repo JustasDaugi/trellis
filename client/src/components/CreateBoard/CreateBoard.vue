74701a408fe2adcb03eb7bbdeb47e460b5e9e134
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-24 15:36:01 +0200

    refactor: refactored code with CloseButton imported

--- client/src/components/CreateBoard/CreateBoard.vue
+++ client/src/components/CreateBoard/CreateBoard.vue
@@ -5,6 +5,7 @@ import { trpc } from '@/trpc'
 import useErrorMessage from '@/composables/useErrorMessage'
 import AlertError from '@/components/AlertError.vue'
 import BackgroundSelection from './BackgroundSelection.vue'
+import CloseButton from './CloseButton.vue'
 
 const router = useRouter()
 const isDialogOpen = ref(false)
@@ -98,29 +99,7 @@ function selectBackground(background: string) {
             >
               Create
             </button>
-
-            <!-- Close Button -->
-            <button
-              type="button"
-              class="absolute right-2 top-2 text-gray-500 hover:text-gray-700 focus:outline-none"
-              @click="closeDialog"
-              aria-label="Close dialog"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+            <CloseButton @close="closeDialog" />
           </form>
         </div>
       </div>
