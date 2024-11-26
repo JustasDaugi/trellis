a7a0a64be003ff799918f6a3e29f7e2bbdf7a56f
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-26 15:39:53 +0200

    refactor: remove pagination parameters

--- server/src/controllers/board/findAll.ts
+++ server/src/controllers/board/findAll.ts
@@ -1,9 +1,7 @@
 import { boardRepository } from '@server/repositories/boardRepository'
 import { publicProcedure } from '@server/trpc'
 import provideRepos from '@server/trpc/provideRepos'
-import { z } from 'zod'
-
-const POSTGRES_INT_MAX = 2147483647
+import { boardSchema } from '@server/entities/board'
 
 export default publicProcedure
   .use(
@@ -12,15 +10,12 @@ export default publicProcedure
     })
   )
   .input(
-    z
-      .object({
-        offset: z.number().int().min(0).max(POSTGRES_INT_MAX).default(0),
-        limit: z.number().int().min(1).max(100).default(20),
-      })
-      .default({})
+    boardSchema.pick({
+      userId: true,
+    })
   )
-  .query(async ({ input: { limit, offset }, ctx: { repos } }) => {
-    const board = await repos.boardRepository.findAll(limit, offset)
+  .query(async ({ input: { userId }, ctx: { repos } }) => {
+    const boards = await repos.boardRepository.findAllByUserId(userId)
 
-    return board
+    return boards
   })
