eb9895d240bef33f32f8e716effd765db1ae6b56
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-27 11:19:39 +0200

    fix: remove css in template for AddListButton container

--- client/src/components/BoardView/AddListButton.vue
+++ client/src/components/BoardView/AddListButton.vue
@@ -9,7 +9,5 @@ defineEmits<{
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center p-4 transition-colors">
     <AddList @list-created="$emit('list-created', $event)" />
-  </div>
 </template>
