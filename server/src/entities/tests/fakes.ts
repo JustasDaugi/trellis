10cc9ff998397ddd62f4284a331a776213d1d255
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-28 13:22:50 +0200

    test: add fakes for testing templates

--- server/src/entities/tests/fakes.ts
+++ server/src/entities/tests/fakes.ts
@@ -1,4 +1,12 @@
-import type { Board, List, Card, User } from '@server/database/types'
+import type {
+  Board,
+  BoardTemplate,
+  List,
+  ListTemplate,
+  Card,
+  CardTemplate,
+  User,
+} from '@server/database/types'
 import type { Insertable } from 'kysely'
 import { random } from '@tests/utils/random'
 import type { AuthUser } from '../user'
@@ -39,7 +47,7 @@ export const fakeAuthUser = <T extends Partial<AuthUser>>(
 export const fakeBoard = <T extends Partial<Insertable<Board>>>(overrides: T) =>
   ({
     title: random.string(),
-    selectedBackground: random.string(),
+    selectedBackground: random.string() || null,
     userId: randomId(),
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -76,3 +84,53 @@ export const fakeCard = <T extends Partial<Insertable<Card>>>(overrides: T) =>
     updatedAt: new Date(),
     ...overrides,
   }) satisfies Insertable<Card>
+
+
+/**
+ * Generates a fake board template with default test data.
+ * @param overrides Any properties that should be different from default fake data.
+ */
+export const fakeBoardTemplate = <T extends Partial<Insertable<BoardTemplate>>>(
+  overrides: T = {} as T
+) =>
+  ({
+    title: random.string(),
+    selectedBackground: overrides.selectedBackground ?? null, // Explicitly handle overrides or default to null
+    userId: randomId(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overrides,
+  }) satisfies Insertable<BoardTemplate>
+
+/**
+ * Generates a fake list template with default test data.
+ * @param overrides Any properties that should be different from default fake data.
+ */
+export const fakeListTemplate = <T extends Partial<Insertable<ListTemplate>>>(
+  overrides: T = {} as T
+) =>
+  ({
+    title: random.string(),
+    order: random.integer({ min: 0, max: 1000 }),
+    boardId: randomId(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overrides,
+  }) satisfies Insertable<ListTemplate>
+
+/**
+ * Generates a fake card template with default test data.
+ * @param overrides Any properties that should be different from default fake data.
+ */
+export const fakeCardTemplate = <T extends Partial<Insertable<CardTemplate>>>(
+  overrides: T = {} as T
+) =>
+  ({
+    title: random.string(),
+    description: random.paragraph(),
+    order: random.integer({ min: 0, max: 1000 }),
+    listId: randomId(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overrides,
+  }) satisfies Insertable<CardTemplate>
