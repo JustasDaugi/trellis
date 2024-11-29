f5b5d8e0f81412efec0000521ad424f0e3cca5af
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-29 15:29:22 +0200

    refactor: remove commented out code for image/paragraph

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -7,12 +7,6 @@ import { isLoggedIn } from '@/stores/user'
   <div class="flex min-h-screen items-center justify-center dark:bg-gray-800">
     <div v-if="!isLoggedIn" class="rounded-md bg-white px-6 py-8">
       <div class="flex flex-col items-center">
-        <p
-          class="mt-4 whitespace-nowrap text-center font-poppins font-bold italic text-gray-500 dark:text-gray-400"
-        >
-          Drive success with your team, anytime.
-        </p>
-
         <div class="mb-40 mt-6 flex flex-col items-center gap-2">
           <FwbButton
             class="w-72 text-center"
@@ -38,16 +32,6 @@ import { isLoggedIn } from '@/stores/user'
 
       <div class="mt-8 flex items-center justify-center">
         <div class="max-w-lg">
-          <!--
-          <picture>
-            <source srcset="../assets/illustration.webp" type="image/webp" />
-            <img
-              class="h-64 w-full rounded-md object-cover object-center"
-              src="../assets/illustration.png"
-              alt="Community illustration"
-            />
-          </picture>
-          -->
         </div>
       </div>
     </div>
