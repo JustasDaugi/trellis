6d13ba23989b206ef5cf57cb55860d89d7e625bc
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-29 14:55:49 +0200

    feat: change authProcedure to public

--- server/src/controllers/card/create.ts
+++ server/src/controllers/card/create.ts
@@ -1,12 +1,11 @@
 import { cardSchema, type CardPublic } from '@server/entities/card'
-import { cardRepository } from '@server/repositories/cardRepository'
-import { listRepository } from '@server/repositories/listRepository'
 import provideRepos from '@server/trpc/provideRepos'
-import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure'
+import { listRepository } from '@server/repositories/listRepository'
+import { cardRepository } from '@server/repositories/cardRepository'
+import { publicProcedure } from '@server/trpc'
 import NotFoundError from '@server/utils/errors/NotFound'
-import ForbiddenError from '@server/utils/errors/Forbidden'
 
-export default authenticatedProcedure
+export default publicProcedure
   .use(
     provideRepos({
       listRepository,
@@ -18,28 +17,19 @@ export default authenticatedProcedure
       .pick({
         listId: true,
         title: true,
+        userId: true,
         description: true,
       })
       .partial({ description: true })
   )
-  .mutation(
-    async ({ input: card, ctx: { repos, authUser } }): Promise<CardPublic> => {
-      const list = await repos.listRepository.findById(card.listId)
+  .mutation(async ({ input: card, ctx: { repos } }): Promise<CardPublic> => {
+    const list = await repos.listRepository.findById(card.listId)
 
-      if (!list) {
-        throw new NotFoundError('List not found')
-      }
-      if (list.userId !== authUser.id) {
-        throw new ForbiddenError(
-          'You are not authorized to create a card in this list'
-        )
-      }
-
-      const newCard = await repos.cardRepository.create({
-        ...card,
-        userId: authUser.id,
-      })
-
-      return newCard
+    if (!list) {
+      throw new NotFoundError('Board not found')
     }
-  )
+
+    const newCard = await repos.cardRepository.create(card)
+
+    return newCard
+  })
