27de1ecb7715e18307c40082e268dd8e0e3489ad
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-11-29 14:57:53 +0200

    feat: change authProcedure to public

--- server/src/controllers/list/create.ts
+++ server/src/controllers/list/create.ts
@@ -1,12 +1,11 @@
 import { listSchema, type ListPublic } from '@server/entities/list'
+import provideRepos from '@server/trpc/provideRepos'
 import { listRepository } from '@server/repositories/listRepository'
 import { boardRepository } from '@server/repositories/boardRepository'
-import provideRepos from '@server/trpc/provideRepos'
-import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure'
+import { publicProcedure } from '@server/trpc'
 import NotFoundError from '@server/utils/errors/NotFound'
-import ForbiddenError from '@server/utils/errors/Forbidden'
 
-export default authenticatedProcedure
+export default publicProcedure
   .use(
     provideRepos({
       boardRepository,
@@ -17,26 +16,17 @@ export default authenticatedProcedure
     listSchema.pick({
       boardId: true,
       title: true,
+      userId: true,
     })
   )
-  .mutation(
-    async ({ input: list, ctx: { authUser, repos } }): Promise<ListPublic> => {
-      const board = await repos.boardRepository.findById(list.boardId)
+  .mutation(async ({ input: list, ctx: { repos } }): Promise<ListPublic> => {
+    const board = await repos.boardRepository.findById(list.boardId)
 
-      if (!board) {
-        throw new NotFoundError('Board not found')
-      }
-      if (board.userId !== authUser.id) {
-        throw new ForbiddenError(
-          'You are not authorized to create a list in this board'
-        )
-      }
-
-      const newList = await repos.listRepository.create({
-        ...list,
-        userId: authUser.id,
-      })
-
-      return newList
+    if (!board) {
+      throw new NotFoundError('Board not found')
     }
-  )
+
+    const newList = await repos.listRepository.create(list)
+
+    return newList
+  })
