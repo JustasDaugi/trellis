0a16a7bc8b47ed1151ccbe17bfddf15bb6126c97
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-12-02 15:51:45 +0200

    refactor: remove pagination feature

--- server/src/controllers/template/getBoards.ts
+++ server/src/controllers/template/getBoards.ts
@@ -1,9 +1,6 @@
 import { templateRepository } from '@server/repositories/templateRepository'
 import { publicProcedure } from '@server/trpc'
 import provideRepos from '@server/trpc/provideRepos'
-import { z } from 'zod'
-
-const POSTGRES_INT_MAX = 2147483647
 
 export default publicProcedure
   .use(
@@ -11,15 +8,7 @@ export default publicProcedure
       templateRepository,
     })
   )
-  .input(
-    z
-      .object({
-        offset: z.number().int().min(0).max(POSTGRES_INT_MAX).default(0),
-        limit: z.number().int().min(1).max(100).default(20),
-      })
-      .default({})
-  )
   .query(async ({ ctx: { repos } }) => {
-    const templates = await repos.templateRepository.findAll()
-    return templates
-  })
+    const templates = await repos.templateRepository.findAll();
+    return templates;
+  });
