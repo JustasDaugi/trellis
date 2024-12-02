f6ce787dd0c6fc555813bd3948a88e1628e57992
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-12-02 15:50:23 +0200

    refactor/style: remove clearTables, keep wrapInRollbacks, format code

--- server/src/controllers/board/tests/boardGet.spec.ts
+++ server/src/controllers/board/tests/boardGet.spec.ts
@@ -2,13 +2,12 @@ import { fakeBoard, fakeUser } from '@server/entities/tests/fakes'
 import { createTestDatabase } from '@tests/utils/database'
 import { createCallerFactory } from '@server/trpc'
 import { wrapInRollbacks } from '@tests/utils/transactions'
-import { clearTables, insertAll } from '@tests/utils/records'
+import { insertAll } from '@tests/utils/records'
 import boardRouter from '..'
 
 const createCaller = createCallerFactory(boardRouter)
 const db = await wrapInRollbacks(createTestDatabase())
 
-await clearTables(db, ['board'])
 const [user] = await insertAll(db, 'user', fakeUser())
 
 const { get } = createCaller({ db })
