e8a1144a52f92cfbe2324418bd7146660e9a0cc2
JustasDaugi <justas.daugininkaitis@gmail.com>
2024-12-03 11:38:09 +0200

    fix: correct custom error name to InternalServerError

--- server/src/utils/errors/InternalServerError.ts
+++ server/src/utils/errors/InternalServerError.ts
@@ -1,6 +1,6 @@
 import { TRPCError } from '@trpc/server'
 
-export default class NotFoundError extends TRPCError {
+export default class InternalServerError extends TRPCError {
   constructor(message: string) {
     super({
       code: 'INTERNAL_SERVER_ERROR',
